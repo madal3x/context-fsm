f997b6e251024d08c0a7354647ac8d6319812625
madal3x <alexandru.madan@conversioncompany.com>
2016-02-01 23:46:36 +0100

    shorter singleton syntax

--- examples/tree/TreeContext.php
+++ examples/tree/TreeContext.php
@@ -26,42 +26,32 @@ class TreeContext extends Context {
     private static $step5;
 
     public static function step1() {
-        if ( ! self::$step1) {
-            self::$step1 = new TreeStep1();
-        }
-
-        return self::$step1;
+        return self::$step1 
+            ? self::$step1 
+            : (self::$step1 = new TreeStep1());
     }
 
     public static function step2() {
-        if ( ! self::$step2) {
-            self::$step2 = new TreeStep2();
-        }
-
-        return self::$step2;
+        return self::$step2 
+            ? self::$step2
+            : (self::$step2 = new TreeStep2());
     }
 
     public static function step3() {
-        if ( ! self::$step3) {
-            self::$step3 = new TreeStep3();
-        }
-
-        return self::$step3;
+        return self::$step3
+            ? self::$step3
+            : (self::$step3 = new TreeStep3());
     }
 
     public static function step4() {
-        if ( ! self::$step4) {
-            self::$step4 = new TreeStep4();
-        }
-
-        return self::$step4;
+        return self::$step4 
+            ? self::$step4 
+            : (self::$step4 = new TreeStep4());
     }
 
     public static function step5() {
-        if ( ! self::$step5) {
-            self::$step5 = new TreeStep5();
-        }
-
-        return self::$step5;
+        return self::$step5 
+            ? self::$step5
+            : (self::$step5 = new TreeStep5());
     }
 }
